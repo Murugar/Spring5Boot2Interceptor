9329097154be46f9da841d1777cd6c1b332fe22f
Davanon <davanon2014@gmail.com>
2018-03-17 10:23:15 +0530

    First

---
+++ src/main/java/com/iqmsoft/config/WebConfig.java
@@ -0,0 +1,16 @@
+package com.iqmsoft.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import com.iqmsoft.interceptor.TestInterceptor;
+
+@Configuration
+public class WebConfig extends WebMvcConfigurerAdapter {
+    
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(new TestInterceptor());
+	}
+    
+}
